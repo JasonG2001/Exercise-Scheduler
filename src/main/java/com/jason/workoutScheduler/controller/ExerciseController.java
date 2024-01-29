c4e585c32f3b7bf9a103ff75e5442c32e07f4c0f
Jason Guan <Jason.Guan21@thehutgroup.com>
2024-01-29 13:35:37 +0000

    Add a controller

--- src/main/java/com/jason/workoutScheduler/controller/ExerciseController.java
+++ src/main/java/com/jason/workoutScheduler/controller/ExerciseController.java
@@ -1,7 +1,6 @@
 package com.jason.workoutScheduler.controller;
 
 import com.jason.workoutScheduler.repository.ExerciseRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
