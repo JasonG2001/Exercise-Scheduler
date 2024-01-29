ff290510b8a2c68faae08e47698b8282b1a2bd00
Jason Guan <Jason.Guan21@thehutgroup.com>
2024-01-29 11:38:45 +0000

    Make repository class and methods

---
+++ src/main/java/com/jason/workoutScheduler/repository/ExerciseRepository.java
@@ -0,0 +1,48 @@
+package com.jason.workoutScheduler.repository;
+
+import com.jason.workoutScheduler.records.Exercise;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.UUID;
+
+public class ExerciseRepository {
+
+    private List<Exercise> exercises;
+
+    public ExerciseRepository() {
+        exercises = Arrays.asList(
+                new Exercise(UUID.randomUUID().toString(), "running", "running on a treadmill", "1", "30"),
+                new Exercise(UUID.randomUUID().toString(), "deadlift", "barbell deadlift", "3", "20")
+        );
+    }
+
+    public List<Exercise> getAll() {
+        return exercises;
+    }
+
+    public Exercise getExerciseById(String id) {
+        return exercises
+                .stream()
+                .filter(exercise -> exercise.id().equals(id)).findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("Exercise not found"));
+    }
+
+    public Exercise createExercise(Exercise newExercise) {
+        exercises.add(newExercise);
+        return newExercise;
+    }
+
+    public void updateExercise(String id, Exercise updatedExercise) {
+        Exercise exercise = getExerciseById(id);
+        int index = exercises.indexOf(exercise);
+        exercises.set(index, updatedExercise);
+    }
+
+    public void deleteExercise(String id) {
+        exercises
+                .stream()
+                .filter(exercise -> !exercise.id().equals(id))
+                .toList();
+    }
+}
