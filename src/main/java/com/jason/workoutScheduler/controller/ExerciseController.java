4c8f23df533264c3bdb56cf3683a484583a1de3f
Jason Guan <Jason.Guan21@thehutgroup.com>
2024-01-30 12:18:52 +0000

    Add all methods to the controller

--- src/main/java/com/jason/workoutScheduler/controller/ExerciseController.java
+++ src/main/java/com/jason/workoutScheduler/controller/ExerciseController.java
@@ -1,8 +1,11 @@
 package com.jason.workoutScheduler.controller;
 
+import com.jason.workoutScheduler.records.Exercise;
 import com.jason.workoutScheduler.repository.ExerciseRepository;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/exercises")
@@ -12,5 +15,33 @@ public class ExerciseController {
 
     public ExerciseController(ExerciseRepository exerciseRepository) {
         this.exerciseRepository = exerciseRepository;
+    }
+
+    @GetMapping
+    public List<Exercise> getAllExercises() {
+        return exerciseRepository.getAll();
+    }
+
+    @GetMapping("/{id}")
+    public Exercise getExerciseById(@PathVariable final String id) {
+        return exerciseRepository.getExerciseById(id);
+    }
+
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping
+    public Exercise postExercise(@RequestBody Exercise exercise) {
+        return exerciseRepository.createExercise(exercise);
+    }
+
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @PutMapping("/{id}")
+    public void updateExercise(@PathVariable final String id, @RequestBody Exercise exercise) {
+        exerciseRepository.updateExercise(id, exercise);
+    }
+
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable String id) {
+        exerciseRepository.deleteExercise(id);
     }
 }
